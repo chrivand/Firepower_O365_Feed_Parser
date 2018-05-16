0bc5363e62c8a90327693945368eaeca2d507bc5
chrivand <chrivand@cisco.com>
2018-05-16 14:00:08 +0200

    added user feedback to script

--- msft-xml-feed-parser.py
+++ msft-xml-feed-parser.py
@@ -1,20 +1,31 @@
 # import objects
 import xml.etree.ElementTree as ET
 import requests
+import sys
 
 # download file from Microsoft and open it for read
 XML_URL = 'https://support.content.office.net/en-us/static/O365IPAddresses.xml'
 r = requests.get(XML_URL, allow_redirects=True)
 XML_File = open('O365IPAddresses.xml', 'r')
 
-# overwrite old files with files or create new the first time (path has to be changed)
-Parsed_File_URL = open('/Users/christophervandermade/Desktop/MicrosoftXMLFeedParser/Parsed_File_URL.txt', 'w+')
-Parsed_File_IPv4 = open('/Users/christophervandermade/Desktop/MicrosoftXMLFeedParser/Parsed_File_IPv4.txt', 'w+')
-Parsed_File_IPv6 = open('/Users/christophervandermade/Desktop/MicrosoftXMLFeedParser/Parsed_File_IPv6.txt', 'w+')
+# user feed back
+sys.stdout.write("\n")
+sys.stdout.write("XML file successfully downloaded!\n") 
+sys.stdout.write("\n")
+
+# overwrite old files with files or create new the first time 
+Parsed_File_URL = open('/Users/christophervandermade/Documents/GitHub/Firepower_O365_Feed_Parser/Parsed_File_URL.txt', 'w+')
+Parsed_File_IPv4 = open('/Users/christophervandermade/Documents/GitHub/Firepower_O365_Feed_Parser/Parsed_File_IPv4.txt', 'w+')
+Parsed_File_IPv6 = open('/Users/christophervandermade/Documents/GitHub/Firepower_O365_Feed_Parser/Parsed_File_IPv6.txt', 'w+')
 
 # parse XML file
 tree = ET.parse(XML_File)
 root = tree.getroot()
+
+# user feed back
+sys.stdout.write("\n")
+sys.stdout.write("XML file successfully parsed!\n") 
+sys.stdout.write("\n")
 
 # initiate lists to be filled with addresses
 URL_List = []
@@ -37,11 +48,26 @@ for addresslist in root.iter('addresslist'):
 for URL in URL_List:
 	Parsed_File_URL.write("%s\n" % URL)
     
+# user feed back
+sys.stdout.write("\n")
+sys.stdout.write("URL .txt file successfully created!\n") 
+sys.stdout.write("\n")
+
 for IPv4 in IPv4_List:
 	Parsed_File_IPv4.write("%s\n" % IPv4)
 
+# user feed back
+sys.stdout.write("\n")
+sys.stdout.write("IPv4 .txt file successfully created!\n") 
+sys.stdout.write("\n")
+
 for IPv6 in IPv6_List:
 	Parsed_File_IPv6.write("%s\n" % IPv6)
+
+# user feed back
+sys.stdout.write("\n")
+sys.stdout.write("IPv6 .txt file successfully created!\n") 
+sys.stdout.write("\n")
 
 # TO DO: upload TXT files to Firepower as objects (either through API, or by hosting online them and use them as Network Object Feed)
 
