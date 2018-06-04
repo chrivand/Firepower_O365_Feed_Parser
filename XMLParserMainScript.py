a9d3b91af962a0b2a4d62ba79ba84218c9d0b960
chrivand <chrivand@cisco.com>
2018-06-04 18:55:31 +0200

    added header disclaimer

--- XMLParserMainScript.py
+++ XMLParserMainScript.py
@@ -1,5 +1,19 @@
 # NOTE: this is a proof of concept script, please test before using in production!
 
+# Python example script showing proper use of the Cisco Sample Code header.
+# Copyright (c) {{current_year}} Cisco and/or its affiliates.
+# This software is licensed to you under the terms of the Cisco Sample
+# Code License, Version 1.0 (the "License"). You may obtain a copy of the
+# License at
+#                https://developer.cisco.com/docs/licenses
+# All use of the material herein must be in accordance with the terms of
+# the License. All rights not expressly granted by the License are
+# reserved. Unless required by applicable law or agreed to separately in
+# writing, software distributed under the License is distributed on an "AS
+# IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied.
+
+
 # import objects
 import xml.etree.ElementTree as ET
 import requests
